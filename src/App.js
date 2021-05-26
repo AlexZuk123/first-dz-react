9dd0a4c171b04faa4613afc6d906b19189d1b3f2
student <student@kr.step>
2021-05-26 20:45:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React</h1>
+      <h2>Жук Александра Сергеевна</h2>
+      <table>
+        <tr><th>Контактный телефон</th><td>067 92 80 323</td></tr>
+        <tr><th>Электронный адрес</th><td><a href="alexzuck123@gmail.com">alexzuck123@gmail.com</a></td></tr>
+      </table>
+      <h2>Кривой Рог</h2>
+      <p>
+      Город основан в 1775 году и расположен на месте слияния рек Ингулец и Саксагань. Седьмой по численности населения город Украины[5], центр Криворожской агломерации. Один из самых длинных городов Европы — линейное расстояние от крайней северной точки до южной 66,1 км по прямой[6]. На официальном сайте исполкома Криворожского городского совета указанная длина города — 126 км[7]. Кривой Рог — важный экономический и научный центр Украины, крупный транспортный узел, центр разработки Криворожского железорудного бассейна.
+      </p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/d/d8/KR_City_collage.png"/>
     </div>
   );
 }
